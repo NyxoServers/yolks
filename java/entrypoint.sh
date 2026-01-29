0138ddcd2378d6149616522520e4f62f57261619
xxcosita3czxx <kokosove18@gmail.com>
2026-01-29 18:38:42 +0100

    java yolks fix

--- java/entrypoint.sh
+++ java/entrypoint.sh
@@ -24,18 +24,13 @@
 # Clear so it shows nicer
 clear
 
-#echo -e "\033[1;39;44m __    .  ..  .       , "
-#echo -e '\033[1;39;44m/  ` _ | _||__| _  __-+-'
-#echo -e "\033[1;39;44m\__.(_)|(_]|  |(_)_)  | "
-#echo -e "\033[1;39;44m                        "
-echo -e               "########################################################"
-echo -e "#\033[1;39;44m    ___      _     _                 _                \033[0m#"
-echo -e "#\033[1;39;44m   / __\___ | | __| | /\  /\___  ___| |_   ___ _   _  \033[0m#"
-echo -e '#\033[1;39;44m  / /  / _ \| |/ _` |/ /_/ / _ \/ __| __| / _ \ | | | \033[0m#'
-echo -e '#\033[1;39;44m / /__| (_) | | (_| / __  / (_) \__ \ |_ |  __/ |_| | \033[0m#'
-echo -e '#\033[1;39;44m \____/\___/|_|\__,_\/ /_/ \___/|___/\__(_)___|\__,_| \033[0m#'
-echo -e "#\033[1;39;44m                                                      \033[0m#"
-echo -e               "########################################################"
+echo -e               "####################################################################"
+echo -e "#\033[1;39;44m     _  __               ____                                     \033[0m#"
+echo -e "#\033[1;39;44m    / |/ /_ ____ _____  / __/__ _____  _____ _______  ___ __ __   \033[0m#"
+echo -e "#\033[1;39;44m   /    / // /\ \ / _ \_\ \/ -_) __/ |/ / -_) __(_-<_/ -_) // /   \033[0m#"
+echo -e "#\033[1;39;44m  /_/|_/\_, //_\_\​\___/___/\__/_/  |___/\__/_/ /___(_)__/\_,_/    \033[0m#"
+echo -e "#\033[1;39;44m       /___/                                                      \033[0m#"
+echo -e               "####################################################################"
 COMMIT_NUMBER=$(cat /commit_count.txt)
 COMMIT_MESSAGE=$(cat /commit_message.txt)
 echo -e " "
@@ -60,42 +55,42 @@ if [ "$ENABLE_AV" = 1 ]; then
 	mkdir -p /home/container/clamav/logs /home/container/clamav/quarantine
 	if find /home/container/clamav/quarantine -type f | grep -q .; then
 		if [ "$AUTOREMOVE" = 1 ]; then
-			printf "\033[1m\033[33mcontainer@coldhost.eu~ \033[1;39;41mQuarantined files are in /clamav/quarantine, Deleting...\033[0m\n"
+			printf "\033[1m\033[33mcontainer@nyxoservers.eu~ \033[1;39;41mQuarantined files are in /clamav/quarantine, Deleting...\033[0m\n"
 			rm -rf /home/container/clamav/quarantine/*
 		else
-			printf "\033[1m\033[33mcontainer@coldhost.eu~ \033[1;39;41mQuarantined files are in /clamav/quarantine, Please delete them to remove this error\033[0m\n"
+			printf "\033[1m\033[33mcontainer@nyxoservers.eu~ \033[1;39;41mQuarantined files are in /clamav/quarantine, Please delete them to remove this error\033[0m\n"
 			exit
 		fi
 	fi
-	echo -e "\033[1m\033[33mcontainer@coldhost.eu~ \033[1;39;44mUpdating Virus Databases...\033[0m"
+	echo -e "\033[1m\033[33mcontainer@nyxoservers.eu~ \033[1;39;44mUpdating Virus Databases...\033[0m"
 	cp /freshclam.conf /home/container/clamav
 	freshclam --config-file=/home/container/clamav/freshclam.conf
 	if [ "$ONLY_PLUGINS" = 1 ]; then
-		echo -e "\033[1m\033[33mcontainer@coldhost.eu~ \033[1;39;44mScanning plugins with ClamAV AntiVirus...\033[0m"
+		echo -e "\033[1m\033[33mcontainer@nyxoservers.eu~ \033[1;39;44mScanning plugins with ClamAV AntiVirus...\033[0m"
 		clamscan -r --move=/home/container/clamav/quarantine --log=/home/container/clamav/logs/clamscan.txt --database=/home/container/clamav/ --infected --include="^[^\.]+$" --include="\.jar$" --exclude-dir="\.cache" --exclude="\.paper-remapped$" /home/container/plugins
 	else
-		echo -e "\033[1m\033[33mcontainer@coldhost.eu~ \033[1;39;44mScanning the home directory with ClamAV AntiVirus...\033[0m"
+		echo -e "\033[1m\033[33mcontainer@nyxoservers.eu~ \033[1;39;44mScanning the home directory with ClamAV AntiVirus...\033[0m"
 		echo -e "THIS MAY TAKE UNDER 10 MINUTES"
 		clamscan -r --move=/home/container/clamav/quarantine --log=/home/container/clamav/logs/clamscan.txt --database=/home/container/clamav/ --infected --include="^[^\.]+$" --include="\.jar$" --exclude-dir="\.cache" --exclude="\.paper-remapped$" /home/container
 	fi
 	if find /home/container/clamav/quarantine -type f | grep -q .; then
 		if [ "$AUTOREMOVE" = 1 ]; then
-			printf "\033[1m\033[33mcontainer@coldhost.eu~ \033[1;39;41mQuarantined files are in /clamav/quarantine, Deleting...\033[0m\n"
+			printf "\033[1m\033[33mcontainer@nyxoservers.eu~ \033[1;39;41mQuarantined files are in /clamav/quarantine, Deleting...\033[0m\n"
 			rm -rf /home/container/clamav/quarantine/*
 		else
-			printf "\033[1m\033[33mcontainer@coldhost.eu~ \033[1;39;41mQuarantined files are in /clamav/quarantine, Please delete them to remove this error\033[0m\n"
+			printf "\033[1m\033[33mcontainer@nyxoservers.eu~ \033[1;39;41mQuarantined files are in /clamav/quarantine, Please delete them to remove this error\033[0m\n"
 			exit
 		fi
 	fi
 else
 	rm -rf /home/container/clamav
-    printf "\033[1m\033[33mcontainer@coldhost.eu~ \033[1;39;44mWARNING: Antivirus scanning is disabled.\n"
+    printf "\033[1m\033[33mcontainer@nyxoservers.eu~ \033[1;39;44mWARNING: Antivirus scanning is disabled.\n"
 fi
 
 # Scan the home directory
 
 # Print Java version
-printf "\033[1m\033[33mcontainer@coldhost.eu~ \033[0mjava -version\n"
+printf "\033[1m\033[33mcontainer@nyxoservers.eu~ \033[0mjava -version\n"
 java -version
 
 # Convert all of the "{{VARIABLE}}" parts of the command into the expected shell
@@ -105,6 +100,6 @@ PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat
 
 # Display the command we're running in the output, and then execute it with the env
 # from the container itself.
-printf "\033[1m\033[33mcontainer@coldhost.eu~ \033[0m%s\n" "$PARSED"
+printf "\033[1m\033[33mcontainer@nyxoservers.eu~ \033[0m%s\n" "$PARSED"
 # shellcheck disable=SC2086
 exec env ${PARSED}
