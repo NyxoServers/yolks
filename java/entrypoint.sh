720e921b42a6856481328f8d0145b9d5ddfda2bf
xxcosita3czxx <kokosove18@gmail.com>
2026-01-30 03:23:14 +0100

    disclaimer

--- java/entrypoint.sh
+++ java/entrypoint.sh
@@ -67,6 +67,7 @@ if [ "$ENABLE_AV" = 1 ]; then
 	freshclam --config-file=/home/container/clamav/freshclam.conf
 	if [ "$ONLY_PLUGINS" = 1 ]; then
 		echo -e "\033[1m\033[33mcontainer@nyxoservers.eu~ \033[1;39;44mScanning plugins with ClamAV AntiVirus...\033[0m"
+		echo -e "THIS MAY TAKE AROUND 2 MINUTES"
 		clamscan -r --move=/home/container/clamav/quarantine --log=/home/container/clamav/logs/clamscan.txt --database=/home/container/clamav/ --infected --include="^[^\.]+$" --include="\.jar$" --exclude-dir="\.cache" --exclude="\.paper-remapped$" /home/container/plugins
 	else
 		echo -e "\033[1m\033[33mcontainer@nyxoservers.eu~ \033[1;39;44mScanning the home directory with ClamAV AntiVirus...\033[0m"
