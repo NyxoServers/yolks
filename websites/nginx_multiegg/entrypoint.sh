ebb1bc0eb0db9aca64659cdef58ee43c9b4b313a
xxcosita3czxx <kokosove18@gmail.com>
2026-02-07 21:13:15 +0100

    nginx egg

--- websites/nginx_multiegg/entrypoint.sh
+++ websites/nginx_multiegg/entrypoint.sh
@@ -1,26 +1,5 @@
-#!/bin/ash
-
-#
-# Copyright (c) 2021 Matthew Penner
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-#
+#!/bin/bash
+sleep 1
 clear
 echo -e               "####################################################################"
 echo -e "#\033[1;39;44m     _  __               ____                                     \033[0m#"
@@ -41,11 +20,9 @@ echo -e " "
 TZ=${TZ:-UTC}
 export TZ
 
-# Set environment variable that holds the Internal Docker IP
 INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
 export INTERNAL_IP
 
-# Switch to the container's working directory
 cd /home/container || exit 1
 
 # Update ClamAV definitions (optional but recommended at runtime)
@@ -87,28 +64,9 @@ else
     printf "\033[1m\033[33mcontainer@nyxoservers.eu~ \033[1;39;44mWARNING: Antivirus scanning is disabled.\n"
 fi
 
-if [ -f "requirements.txt" ]; then
-	echo -e "\033[1m\033[ Installing dependencies with pip...\033[0m\n"
-	pip install --no-cache-dir -r requirements.txt
-fi
+# Replace Startup Variables
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+echo ":/home/container$ ${MODIFIED_STARTUP}"
 
-if [ -z "$MAIN" ] || [ "$MAIN" == "" ]; then
-	MAIN="bot.py"
-fi
-
-# Print Python version
-printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0mpython --version\n"
-python --version
-
-printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0mpip --version\n"
-pip --version
-# Convert all of the "{{VARIABLE}}" parts of the command into the expected shell
-# variable format of "${VARIABLE}" before evaluating the string and automatically
-# replacing the values.
-PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
-
-# Display the command we're running in the output, and then execute it with the env
-# from the container itself.
-printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0m%s\n" "$PARSED"
-# shellcheck disable=SC2086
-exec env ${PARSED}
+# Run the Server
+${MODIFIED_STARTUP}
