0734010cb53c9a1a6138ce85ada91d39835902e6
xxcosita3czxx <kokosove18@gmail.com>
2026-02-07 21:16:55 +0100

    fix nginx egg

--- websites/nginx_multiegg/webroot/index.php
+++ websites/nginx_multiegg/webroot/index.php
@@ -101,9 +101,6 @@ $phpVersion = phpversion();
     <div class="php-version"><?php echo $phpVersion; ?></div>
     <div class="message">Webserver correctly installed and running!</div>
     <div class="webroot-message">You can now place your website files in the webroot folder</div>
-    <div class="footer">
-        <p>Powered by <a href="https://sigmaprods.dev" target="_blank">sigmaprods.dev</a></p>
-    </div>
 </div>
 
 </body>
